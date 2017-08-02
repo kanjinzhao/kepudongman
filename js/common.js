66bdc868df12e02b3f759a7ba79d3f86ec66dab8
kanjinzhao <540032146@qq.com>
2017-08-02 09:31:10 +0800

    8月2号更新

--- js/common.js
+++ js/common.js
@@ -1,60 +1,61 @@
-$(function ($) {
-    // 倒计时
-    function ShowCountDown(year, month, day, divname) {
-        var now = new Date();
-        var endDate = new Date(year, month, day);
-        var leftTime = endDate.getTime() - now.getTime();
-        var dd = parseInt(leftTime / 1000 / 60 / 60 / 24, 10);//计算剩余的天数
-        var hh = parseInt(leftTime / 1000 / 60 / 60 % 24, 10);//计算剩余的小时数
-        var mm = parseInt(leftTime / 1000 / 60 % 60, 10);//计算剩余的分钟数
-        var ss = parseInt(leftTime / 1000 % 60, 10);//计算剩余的秒数
-        dd = checkTime(dd);
-        hh = checkTime(hh);
-        mm = checkTime(mm);
-        ss = checkTime(ss);//小于10的话加0
+$(function($){
+  // 倒计时
+      function ShowCountDown(year,month,day,divname)
+      {
+          var now = new Date();
+          var endDate = new Date(year, month, day);
+          var leftTime=endDate.getTime()-now.getTime();
+          var dd = parseInt(leftTime / 1000 / 60 / 60 / 24, 10);//计算剩余的天数
+          var hh = parseInt(leftTime / 1000 / 60 / 60 % 24, 10);//计算剩余的小时数
+          var mm = parseInt(leftTime / 1000 / 60 % 60, 10);//计算剩余的分钟数
+          var ss = parseInt(leftTime / 1000 % 60, 10);//计算剩余的秒数
+          dd = checkTime(dd);
+          hh = checkTime(hh);
+          mm = checkTime(mm);
+          ss = checkTime(ss);//小于10的话加0
 
-        $("#djs01").html(dd);
-        $("#djs02").html(hh);
-        $("#djs03").html(mm);
-    }
+          $("#djs01").html(dd);
+          $("#djs02").html(hh);
+          $("#djs03").html(mm);
+      }
+      function checkTime(i)
+      {
+          if (i < 10) {
+              i = "0" + i;
+          }
+          return i;
+      }
 
-    function checkTime(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
-    }
-
-    // 设置时间
-    ShowCountDown(2017, 11, 30, 'countdown');
+      // 设置时间
+        ShowCountDown(2017,11,30,'countdown');
 // 参赛旋转
-    var zhuang = 0;
-    setInterval(function () {
-        zhuang++;
-        $('.gundong').css({
-            transform: 'rotate(' + zhuang + 'deg)'
-        })
-        if (zhuang == 360) {
-            zhuang = 0
-        }
-    }, 100)
+var zhuang=0;
+setInterval(function(){
+  zhuang++;
+  $('.gundong').css({
+    transform:'rotate('+ zhuang +'deg)'
+  })
+  if (zhuang == 360) {
+    zhuang = 0
+  }
+},100)
 
 // 列表页
 
-    var $tab_li = $('#tabb ul li');
-    // var ulWidth = -$('#tabb .tab_menu').width()/2;
-    // console.log(ulWidth);
-    // $('#tabb .tab_menu').css({
-    // 	"margin-left":ulWidth
-    // })
-    $tab_li.click(function () {
-        $(this).addClass('selected').siblings().removeClass('selected');
-        var index = $tab_li.index(this);
-        $('div.tab_box > div').eq(index).show().siblings().hide();
-    });
+var $tab_li = $('#tabb ul li');
+				// var ulWidth = -$('#tabb .tab_menu').width()/2;
+				// console.log(ulWidth);
+				// $('#tabb .tab_menu').css({
+				// 	"margin-left":ulWidth
+				// })
+				$tab_li.click(function(){
+						$(this).addClass('selected').siblings().removeClass('selected');
+						var index = $tab_li.index(this);
+						$('div.tab_box > div').eq(index).show().siblings().hide();
+					});
 
-    $('.dianzan').click(function () {
-        $(this).attr('src', 'imgs/zan.png');
+    $('.dianzan').click(function() {
+      $(this).attr('src','imgs/zan.png');
     });
 
 
@@ -62,44 +63,45 @@ $(function ($) {
     //     $('.fen_').show();
     //     console.log("11");
     // })
-    $('.fen_').hover(
-        function () {
+          $('.fen_').hover(
+          function () {
 
             $(this).find('.bdsharebuttonbox').addClass("hover_");
-        },
-        function () {
+          },
+          function () {
             $(this).find('.bdsharebuttonbox ').removeClass("hover_");
-        }
-    );
+          }
+      );
     // $('.fx_').click(function() {
     //
     // });
     $(".ph_cont li:even").css({
-        'backgroundColor': "#DCDDDD"
+      'backgroundColor':"#DCDDDD"
     })
+
 
 
     //锚点
 
-    var ua = navigator.userAgent;
+      var ua = navigator.userAgent;
 
-    var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+      var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
 
-        isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+      isIphone =!ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
 
-        isAndroid = ua.match(/(Android)\s+([\d.]+)/),
+      isAndroid = ua.match(/(Android)\s+([\d.]+)/),
 
-        isMobile = isIphone || isAndroid;
+      isMobile = isIphone || isAndroid;
 
-    //判断
+      //判断
 
-    if (isMobile) {
-        $("._cs").click(function () {
-            $('html,body').animate({scrollTop: 615}, 500)
+      if(isMobile){
+        $("._cs").click(function(){
+          $('html,body').animate({scrollTop: 615}, 500)
         })
-    } else {
-        $("._cs").click(function () {
-            $('html,body').animate({scrollTop: 1715}, 500)
+      }else{
+        $("._cs").click(function(){
+          $('html,body').animate({scrollTop: 1715}, 500)
         })
-    }
+      }
 })
--- js/index.js
+++ js/index.js
@@ -1,66 +1,69 @@
+
+
+
 //var slide={i:2};
 
 
-$(function () {
-    var focus = window.focus || {};
-    var timer = 5000;
-    var _w = 1300;
-    var slideBox = $('.slideShow');
-    var picList = $('.slideShow li[class^="pic"]');
-    var picLength = picList.length / 2;
-    picList.each(function (i, o) {
-        $('.pic' + (i + 1)).css({
-            background: 'url(' + focus['pic' + (i + 1)] + ')',
-            float: 'left',
-            height: '415px',
-            margin: '0 50px',
-            opacity: 1,
-            width: '1200px'
+$(function(){
+        var focus = window.focus || {};
+        var timer = 5000;
+        var _w = 1300;
+        var slideBox = $('.slideShow');
+        var picList = $('.slideShow li[class^="pic"]');
+        var picLength = picList.length/2;
+        picList.each(function(i,o){
+            $('.pic'+(i+1)).css({
+                background : 'url('+focus['pic'+(i+1)] +')',
+                float: 'left',
+                height: '415px',
+                margin: '0 50px',
+                opacity: 1,
+                width: '1200px'
 
-        });
-    });
-    slideBox.css({
-        left: -_w * picLength + 'px',
-        width: _w * picLength * 2 + 'px'
-    });
-    var make = function () {
-    }
-    var st1;
-    var move = function (i) {
-        window.clearTimeout(st1);
-        if (i > picLength) {
-            i = 1;
-        }
-        var l = -_w * (i - 1);
-        $('.slide_unchose').removeClass('slide_on');
-        $('.slide_unchose').eq(i - 1).addClass('slide_on');
-        $('.mask').stop(true).animate({opacity: 0.3}, 100, function () {
-            $('.slideShow').stop(true).animate({
-                left: -_w * (picLength - 1 + i) + 'px'
-            }, function () {
-
-                if (i > picLength - 1) {
-                    $('.slideShow').css({
-                        left: -_w * (i - 1) + 'px'
-                    });
-                }
-                $('.mask').stop(true).animate({opacity: 0}, 500);
-                $('.slide_unchose').removeClass('slide_on');
-                $('.slide_unchose').eq(i - 1).addClass('slide_on');
             });
         });
-        var arg = arguments;
-        st1 = window.setTimeout(function () {
-            arg.callee(++i);
-        }, timer)
+        slideBox.css({
+            left: -_w*picLength + 'px',
+            width : _w*picLength*2 + 'px'
+        });
+       var make = function(){
+       }
+        var st1;
+        var move = function(i){
+            window.clearTimeout(st1);
+            if(i > picLength) {
+                i = 1;
+            }
+            var l = -_w*(i-1);
+            $('.slide_unchose').removeClass('slide_on');
+            $('.slide_unchose').eq(i-1).addClass('slide_on');
+            $('.mask').stop(true).animate({opacity:0.3},100,function(){
+                $('.slideShow').stop(true).animate({
+                    left : -_w*(picLength-1 + i) + 'px'
+                },function(){
 
-    }
-    st1 = window.setTimeout(function () {
-        move(2);
-    }, timer);
-    $('.ap_out').delegate('li.slide_unchose', 'click', function (e) {
-        e.preventDefault();
-        var index = $('.ap_out li.slide_unchose').index($(this)) + 1;
-        move(index);
-    });
-});
+                    if(i > picLength-1) {
+                        $('.slideShow').css({
+                            left: -_w*(i-1) + 'px'
+                        });
+                    }
+                    $('.mask').stop(true).animate({opacity:0},500);
+                    $('.slide_unchose').removeClass('slide_on');
+                    $('.slide_unchose').eq(i-1).addClass('slide_on');
+                });
+            });
+            var arg = arguments;
+            st1 = window.setTimeout(function(){
+                arg.callee(++i);
+            },timer)
+
+        }
+        st1 = window.setTimeout(function(){
+            move(2);
+        },timer);
+        $('.ap_out').delegate('li.slide_unchose','click',function(e){
+            e.preventDefault();
+            var index = $('.ap_out li.slide_unchose').index($(this)) + 1;
+            move(index);
+        });
+	});
